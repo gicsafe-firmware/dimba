9f8d22ce8dea6c2c8b57ee6c0216cf8927f21e88
Dario Baliña <dariosb@gmail.com>
2018-06-04 11:55:11 -0300

    conmgr definitions

--- dimba/conmgr/conmgr.h
+++ dimba/conmgr/conmgr.h
@@ -30,46 +30,84 @@ extern "C" {
 
 /* --------------------------------- Macros -------------------------------- */
 /* -------------------------------- Constants ------------------------------ */
-#define MOVISTAR_APN_ADDR   "INTERNER.GPRS.UNIFON.COM.AR"  
-#define MOVISTAR_APN_USER   "WAP"
-#define MOVISTAR_APN_PASS   "WAP"
 
-#define CLARO_APN_ADDR      "INTERNET.CTIMOVIL.COM.AR"
-#define CLARO_APN_USER      "CLAROGPRS"
-#define CLARO_APN_PASS      "CLAROGPRS999"
-
+/**
+ * Specifies time waiting for GSM modem first response.
+ */
 #define SYNC_DELAY_TIME     RKH_TIME_MS(1000)
-#define MAX_SYNC_RETRY      5
-#define SIM_PIN_NUMBER      9474
 
+/**
+ * Specifies the maximum tries seeking for GSM modem response. 
+ */
+#define MAX_SYNC_RETRY      5
+
+/**
+ * Specifies time waiting for GSM modem network registration.
+ */
 #define REGISTRATION_TIME   RKH_TIME_MS(60000)
 
+/**
+ * Specifies a time delay after general failure before to restart connection.
+ */
 #define FAILURE_TRY_DELAY   RKH_TIME_MS(10000)
 
+/**
+ * Specifies the maximum tries for setting APN configuration.
+ */
 #define MAX_CONFIG_RETRY    5
+
+/**
+ * Specifies a time delay after config error before to restart connection.
+ */
 #define CONFIG_TRY_DELAY    RKH_TIME_MS(3000)
 
-#define CONNECTION_APN      CLARO_APN_ADDR
-#define CONNECTION_USER     CLARO_APN_USER
-#define CONNECTION_PASSWORD CLARO_APN_PASS
-
-#define CONNECTION_PROT     "TCP"
-
-#define YIPIES_SERVER       "71.6.135.77"
-#define YIPIES_PORT         "33499"
-
-#define DOCKLIGHT_SERVER    "181.230.72.86"
-#define DOCKLIGHT_PORT      "33498"
-
-#define DOCKLIGHT_BIS_SERVER    "200.127.3.140"
-#define DOCKLIGHT_BIS_PORT      "33498"
-
-#define CONNECTION_DOMAIN   DOCKLIGHT_SERVER 
-#define CONNECTION_PORT     DOCKLIGHT_PORT
-
+/**
+ * Specifies maximum server connection attemps.
+ */
 #define MAX_CONNECT_RETRY   3
+
+/**
+ * Specifies a time delay between server connection attemps.
+ */
 #define CONNECT_TRY_DELAY   RKH_TIME_MS(5000)
+
+/**
+ * Specifies connections status´s polling period.
+ */
 #define CONNSTATUS_PERIOD   RKH_TIME_MS(2000)
+
+/**
+ * GSM Network Provider and Connection specific configurations.
+ */
+/* .................................. APN .................................. */
+    #define MOVISTAR_APN_ADDR   "INTERNER.GPRS.UNIFON.COM.AR"  
+    #define MOVISTAR_APN_USER   "WAP"
+    #define MOVISTAR_APN_PASS   "WAP"
+
+    #define CLARO_APN_ADDR      "INTERNET.CTIMOVIL.COM.AR"
+    #define CLARO_APN_USER      "CLAROGPRS"
+    #define CLARO_APN_PASS      "CLAROGPRS999"
+
+#define CONNECTION_APN          CLARO_APN_ADDR
+#define CONNECTION_USER         CLARO_APN_USER
+#define CONNECTION_PASSWORD     CLARO_APN_PASS
+
+/* .................................. SIM PIN .............................. */
+#define SIM_PIN_NUMBER      9474
+
+/* ................................. Server ................................ */
+    #define YIPIES_SERVER       "71.6.135.77"
+    #define YIPIES_PORT         "33499"
+
+    #define DOCKLIGHT_SERVER    "181.230.72.86"
+    #define DOCKLIGHT_PORT      "33498"
+
+    #define DOCKLIGHT_BIS_SERVER "200.127.3.140"
+    #define DOCKLIGHT_BIS_PORT   "33498"
+
+#define CONNECTION_PROT         "TCP"
+#define CONNECTION_DOMAIN       DOCKLIGHT_SERVER 
+#define CONNECTION_PORT         DOCKLIGHT_PORT
 
 /* ................................ Signals ................................ */
 /* ........................ Declares active object ......................... */
