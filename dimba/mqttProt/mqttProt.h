5e969879024cf79e219b94014224ee69e51a92ee
lf <francuccilea@gmail.com>
2018-06-19 13:48:44 -0300

    Changed publising time to 1 minute only for testing

--- dimba/mqttProt/mqttProt.h
+++ dimba/mqttProt/mqttProt.h
@@ -31,7 +31,7 @@ extern "C" {
 /* --------------------------------- Macros -------------------------------- */
 /* -------------------------------- Constants ------------------------------ */
 #define WAIT_CONNECT_TIME       RKH_TIME_MIN(1)
-#define PUBLISH_TIME            /*RKH_TIME_SEC(30)*/ RKH_TIME_MIN(3)
+#define PUBLISH_TIME            /*RKH_TIME_SEC(30)*/ RKH_TIME_MIN(1)
 #define SYNC_TIME               RKH_TIME_SEC(5)
 
 /* ................................ Signals ................................ */
