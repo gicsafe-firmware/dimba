9a5aaebfddc6bbf58903b88365b57e6255569377
lf <francuccilea@gmail.com>
2019-03-25 13:39:25 -0300

    Fixed invalid time from RTC

--- dimba/epoch/src/epoch.c
+++ dimba/epoch/src/epoch.c
@@ -31,6 +31,8 @@
 
 /* ----------------------------- Local macros ------------------------------ */
 /* ------------------------------- Constants ------------------------------- */
+static const Time dftTime = {0, 0, 0, 1, 1, 1970, 3, 0};
+
 /* ---------------------------- Local data types --------------------------- */
 typedef unsigned long UL;
 typedef int MInt;
@@ -47,13 +49,33 @@ static short year;
 
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
+static int
+verifyTime(Time *tm)
+{
+    int isValid = 0;
+
+    if ((tm->tm_sec > 59) ||
+        (tm->tm_min > 59) ||
+        (tm->tm_hour > 24) ||
+        ((tm->tm_mday == 0) || (tm->tm_mday > 31)) ||
+        ((tm->tm_mon == 0) || (tm->tm_mon > 12)) ||
+        (tm->tm_year < 1970) ||
+        ((tm->tm_wday == 0) || (tm->tm_wday > 8)))
+    {
+        isValid = 1;
+    }
+    return isValid;
+}
+
 static Epoch
 updStep_start(void)
 {
+    int isValid;
     Time *currTime;
 
     currTime = rtime_get();
-    nextTime = *currTime;
+    isValid = verifyTime(currTime);
+    nextTime = (isValid == 0) ? *currTime : (Time)dftTime;
     updatingEpoch = 0;
     return updatingEpoch;
 }
@@ -154,9 +176,15 @@ epoch_get(void)
 void
 epoch_updateNow(void)
 {
+    int isValid;
     Time *currTime;
 
     currTime = rtime_get();
+    isValid = verifyTime(currTime);
+    if (isValid == 1)
+    {
+        currTime = (Time *)&dftTime;
+    }
     currEpoch = epoch_mktime(currTime);
 }
 
